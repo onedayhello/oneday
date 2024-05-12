c09f6743c2988b5c0b26373a18b7cfee15e33fb8
arbrsts <136247191+arbrsts@users.noreply.github.com>
2024-05-12 20:43:23 +0100

    feat: add actions tab with css subgrid :)

--- app/src/app/dashboard/journals/page.tsx
+++ app/src/app/dashboard/journals/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button } from "@/components/button/Button";
+import { Dropdown } from "@/components/input/Dropdown";
+import { TextInput } from "@/components/input/TextInput";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "next/link";
@@ -41,15 +43,35 @@ const Tiptap = () => {
 const Journal = () => {
   return (
     <div className="flex flex-col gap-3">
-      <Link href="/dashboard/journals/new-entry">
-        <Button>New Entry</Button>
-      </Link>
+      <div className="border rounded-lg gap-1 lg:col-span-5 p-5  ">
+        <div className="grid grid-cols-[1fr_1fr_1fr_auto] mb-2 gap-4">
+          <span>Thought</span>
+          <span>Type of thinking</span>
+          <span>Better thought</span>
+          <span>Actions</span>
+          <div className="relative grid grid-cols-subgrid items-top col-span-4">
+            <TextInput />
+            <Dropdown />
+            <TextInput />
+            <div className="flex gap-2 justify-center items-center">
+              <Button variant="secondary">-</Button>
+              <Button>+</Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-2">
+          <Button>Submit</Button>
+        </div>
+      </div>
 
       <div className="flex flex-col gap-2">
         <RecentJournalsItem />
         <RecentJournalsItem />
         <RecentJournalsItem />
       </div>
+
+
     </div>
   );
 };
